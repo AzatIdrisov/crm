34175288b272136000244f41aea9c337f742584c
AzatIdrisov <azzzat72@yandex.ru>
2026-03-22 15:04:21 +0500

    update .gitignore, project structure

---
+++ src/main/java/com/crm/CrmApplication.java
@@ -0,0 +1,20 @@
+package com.crm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableAsync
+@EnableScheduling
+@EnableCaching
+@ConfigurationPropertiesScan
+public class CrmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrmApplication.class, args);
+    }
+}
